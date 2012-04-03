8a1b225a4fd2062eeb76fd0c2332561f81b4b18d
Aziz Khoury <bentael@gmail.com>
2012-04-03 18:30:16 -0400

    adding ga

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,6 @@
 Trll::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb
-
+  GA.tracker = "UA-30582764-1"
   # Code is not reloaded between requests
   config.cache_classes = true
 
