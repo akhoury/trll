c34eccd7dca96341e6a1fea992cb046f39628d52
Aziz Khoury <bentael@gmail.com>
2012-04-04 16:51:08 -0400

    mergin old records

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120404172231) do
+ActiveRecord::Schema.define(:version => 20120404204825) do
 
   create_table "urls", :force => true do |t|
     t.string   "url",                           :null => false
